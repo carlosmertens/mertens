06cddb6c053fc82fff47b71f2d8069fa59804725
Carlos Mertens <mertenscarlos@yahoo.com>
2022-11-30 13:37:59 +0100

    Refactor: Move banner to Header component

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className='banner-box'>
+        <h1 className='banner-title'>
+          <span>carlos mertens</span>
+          <span className='banner-title-sub'>
+            your exiting and creative web developer
+          </span>
+        </h1>
+      </div>
+    </header>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
@@ -12,11 +13,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <div className='title-display'>
-          <h1>CARLOS MERTENS</h1>
-        </div>
-      </main>
+      <Header />
     </div>
   );
 }
