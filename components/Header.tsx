8d2fa52b35dd9df3efbe006c1c8275ac23f43eb8
Carlos Mertens <mertenscarlos@yahoo.com>
2022-12-01 12:54:20 +0100

    feat: add tailwindcss and google font

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className='header'>
-      <nav className='nav-container'>
+    <header className='relative h-screen flex'>
+      <nav className='absolute top-0 left-0 h-1/6 w-full flex items-center'>
         <Link
-          className='nav-logo-link git-animated'
+          className='ml-10 git-animated'
           href='https://github.com/carlosmertens'>
           <Image
             className='nav-logo'
@@ -17,7 +17,7 @@ export const Header = () => {
           />
         </Link>
         <Link
-          className='nav-logo-link twit-animated'
+          className='ml-4 twit-animated'
           href='https://twitter.com/CarlosMertens1'>
           <Image
             className='nav-logo'
@@ -28,7 +28,7 @@ export const Header = () => {
           />
         </Link>
         <Link
-          className='nav-logo-link in-animated'
+          className='ml-4 in-animated'
           href='https://www.linkedin.com/in/carlosmertens/'>
           <Image
             className='nav-logo'
@@ -40,10 +40,10 @@ export const Header = () => {
         </Link>
       </nav>
 
-      <div className='banner-container'>
-        <h1 className='banner-title'>
-          <span>carlos mertens</span>
-          <span className='banner-title-sub'>
+      <div className='w-full flex justify-center items-center text-center flex-col'>
+        <h1 className='banner-title text-3xl sm:text-5xl lg:text-7xl'>
+          <span>Carlos mertens</span>
+          <span className='hidden'>
             your exiting and creative web developer
           </span>
         </h1>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Zen_Dots } from '@next/font/google';
+
+const zen = Zen_Dots({
+  weight: '400',
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={zen.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
